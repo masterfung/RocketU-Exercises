9610c1b53828f92473f25fc39573914999c1cbfb
Tsung Hung <hungmasterj@gmail.com>
2014-06-19 16:17:31 -0700

    ben and johnny collab
    it was joy and joy of fun! and confusion

--- Blog/main.py
+++ Blog/main.py
@@ -30,11 +30,29 @@ def blog_manager(user, blog_posts, authors):
 	elif action == 'q':
 		print 'Have a good day!'
 	elif action == 'h':
-		for index,post in blog_posts:
-			print 'This is the title: {}.'.format(post)
-			print 'Content: {}\n'.format(post.content)
+		for index,post in enumerate(blog_posts):
+			print 'This is the title: {}. Reference: {}.'.format(post, index+1)
+
+		user_num = int(raw_input('What is the index number? '))-1
+
+		follow_up = raw_input('What would you like to change: (d)ate, (a)uthor, (t)itle, or (c)ontent ').lower()
+		if follow_up == 'd':
+			new_date = raw_input('What is the new year? ')
+			blog_posts[user_num].published_date = new_date
+		elif follow_up == 'a':
+			author_name = raw_input('What is your name? ')
+			author = Author(author_name)
+			authors.append(author)
+			blog_posts[user_num].change_author(author)
+		elif follow_up == 't':
+			new_title = raw_input('New title name: ')
+			blog_posts[user_num].title = new_title
+		elif follow_up == 'c':
+			new_content = raw_input('New content: ')
+			blog_posts[user_num].content = new_content
 	else:
 		print 'Please select correctly from above.'
+	blog_manager(user, blog_posts, authors)
 
 print 'Hey! Welcome to the blog manager {}.'.format(sys.argv[1]) #sys.argv[0] is the file name
 
