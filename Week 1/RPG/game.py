8fa4428dff2e9a2b66391773a5172c1400dfd9ca
Tsung Hung <hungmasterj@gmail.com>
2014-06-21 19:53:39 -0700

    rpg evolving

--- Week 1/RPG/game.py
+++ Week 1/RPG/game.py
@@ -11,11 +11,14 @@ class Nirvana(object):
 	def __repr__(self):
 		return 'Howdy! {}'.format(self.name)
 
-	def character(self):
-		print 'Welcome to the Nirvana game. {}, will face a lot challenges and evil.'.format(self.name)
+	def game_play(self):
+		print 'Welcome to the Nirvana game. {}, you will face a lot challenges and evil. Be warned'.format(self.name)
 		user_response = raw_input('Would you like to pursue the quest to nirvana? (Y/N) ').lower()
+		hit_points = 3
+		enlightenment
 		while user_response != 'n':
-			pass
+			hit_points = 3
+
 
 
 	def demons(self):
@@ -23,16 +26,20 @@ class Nirvana(object):
 			['Clancy the Good', 'Terrance the Brave', 'Jorge the Trustworthy', 'Penguina the Prudent', 'Susie the Fair', 'Maggie the Kind'],
 				  ['Sauron the Greedy', 'Phillip Morris the Kingslayer', 'Hittles the Adolf of Men Doom', 'Yvette the Evil', 'Maleficent the malicious', 'Alexandra the Whore']
 		]
-		demon_type = random.choice(demons)
-		demon = random.choice(demon_type)
+		demon_type = randint(0,1)
+		demon = random.choice(demons[demon_type])
 		return demon
 
 
 
 	def mystery_box(self):
 		mystery_present = [
-			'angelic dust', 'holy water', ''
+			['angelic dust', 'holy water', 'blessed sword', 'protection spell', 'invisibility cloak', 'antibiotics'],
+			['death spell', 'rabies', 'wolves', 'decapitated', 'internal bleeding', 'dysentery']
 		]
+		mystery_random = randint(0,1)
+		mystery = random.choice(mystery_present[mystery_random])
+		return mystery
 
 	def random_event_generator(self):
 		pass
@@ -41,6 +48,7 @@ class Nirvana(object):
 		seven_sins = [ 'envy', 'gluttony', 'pride', 'wrath',
 					   'lust', 'sloth', 'greed']
 		sins = random.choice(seven_sins)
+		return sins
 
 	def reincarnation(self):
 		'''
@@ -49,4 +57,6 @@ class Nirvana(object):
 		pass
 
 	def destinations(self):
-		pass
+		locations = ['Chicago', 'New York City', 'Vancouver', 'Paris', 'Tibet',
+					 'Melbourne', 'Moscow', 'Rome', 'Tokyo', 'Taipei', 'Beijing',
+					 '']
--- Week 1/RPG/main.py
+++ Week 1/RPG/main.py
@@ -1,1 +1,6 @@
+from game import Nirvana
+
 __author__ = 'htm'
+
+def nirvana_play():
+	pass
