b1e14bde6e397112ff8d16ae392c2fbade7834f2
Tsung Hung <hungmasterj@gmail.com>
2014-07-16 17:17:13 -0700

    rocking it good with js hw!

--- Week 5/jquery_base/index.js
+++ Week 5/jquery_base/index.js
@@ -1,3 +1,45 @@
 $(document).ready(function() {
+    var total = [];
+
+    $(".tellMore").on('click', function() {
+        $('.moreInfo').css('display', 'inline');
+    });
+
+
+    $("#1").on('click', function() {
+        $("#ancient").clone().appendTo('.cart');
+        $("#price-1").clone().appendTo('.cart');
+        total.push(parseInt($('#price-1').text()))
+        console.log(total);
+    });
+
+    $("#2").on('click', function() {
+        $('#exlir').clone().appendTo('.cart');
+        $("#price-2").clone().appendTo('.cart');
+        total.push(parseInt($('#price-2').text()))
+        console.log(total);
+    });
+
+    $("#3").on('click', function() {
+        $('#truthEye').clone().appendTo('.cart');
+        $("#price-3").clone().appendTo('.cart');
+        total.push(parseInt($('#price-3').text()))
+        console.log(total);
+    });
+
+    $(".checkout").on('click', function() {
+        finalSum = 0;
+        for (var i = 0; i < total.length; i++) {
+            finalSum += total[i];
+        }
+
+        $('.cart').toggle("hide");
+        $(".checkout-box").css("display", 'inline');
+
+        alert("Your total is: $" + finalSum + ".00 Okta");
+    });
+
+
 
 });
+
