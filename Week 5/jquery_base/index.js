9b21092e730c57bc3dcc052ca6fe2fe3aaf62edc
Tsung Hung <hungmasterj@gmail.com>
2014-07-17 14:38:53 -0700

    advancing shopping logic

--- Week 5/jquery_base/index.js
+++ Week 5/jquery_base/index.js
@@ -5,42 +5,76 @@ $(document).ready(function() {
         $('.moreInfo').css('display', 'inline');
     });
 
+    $('div.product').hover(function(){
+        $(this).toggleClass('active');
+    });
+
+    $('.discount').on('click', function(){
+        var x = 15 * 0.9;
+        $('#price-1').text('$' + x);
+    });
 
     $("#1").on('click', function() {
-        $("#ancient").clone().appendTo('.cart');
-        $("#price-1").clone().appendTo('.cart');
-        total.push(parseInt($('#price-1').text()))
-        console.log(total);
+        count = 0;
+        var x = confirm('Are you sure you want to add this product?')
+        if (x == true) {
+            $("#ancient").clone().appendTo('.cart');
+            count++;
+            total.push(parseInt($('#price-1').text()));
+            console.log(total);
+        }
     });
 
     $("#2").on('click', function() {
         $('#exlir').clone().appendTo('.cart');
         $("#price-2").clone().appendTo('.cart');
-        total.push(parseInt($('#price-2').text()))
+        total.push(parseInt($('#price-2').text()));
         console.log(total);
     });
 
     $("#3").on('click', function() {
         $('#truthEye').clone().appendTo('.cart');
         $("#price-3").clone().appendTo('.cart');
-        total.push(parseInt($('#price-3').text()))
+        total.push(parseInt($('#price-3').text()));
         console.log(total);
     });
 
     $(".checkout").on('click', function() {
-        finalSum = 0;
-        for (var i = 0; i < total.length; i++) {
-            finalSum += total[i];
+        var x = confirm('Do you want to checkout?');
+        if (x == true) {
+            finalSum = 0;
+            for (var i = 0; i < total.length; i++) {
+                finalSum += total[i];
+            }
+
+            $('.cart').toggle("hide");
+            $(".checkout-box").css("display", 'inline');
+            $('.checkout-box').prepend("Final Total: $" + finalSum);
+
+            alert("Your total is: $" + finalSum + ".00 Okta");
         }
 
-        $('.cart').toggle("hide");
-        $(".checkout-box").css("display", 'inline');
-        $('.checkout-box').prepend("Final Total: $" + finalSum);
-
-        alert("Your total is: $" + finalSum + ".00 Okta");
     });
 
 
 
 });
 
+//function confirmation() {
+//        var x;
+//        if (confirm('Do you want to proceed to checkout?') == true) {
+//
+//            finalSum = 0;
+//            for (var i = 0; i < total.length; i++) {
+//                finalSum += total[i];
+//            }
+//
+//            $('.cart').toggle("hide");
+//            $(".checkout-box").css("display", 'inline');
+//            $('.checkout-box').prepend("Final Total: $" + finalSum);
+//
+//            alert("Your total is: $" + finalSum + ".00 Okta");
+//        } else {
+//            x = 'Come back when you are ready';
+//        }
+//    }
--- Week 5/w5d4.js
+++ Week 5/w5d4.js
@@ -2,6 +2,7 @@
  * Created by htm on 7/17/14.
  */
 
+//first set of how
 $('li').css('color', 'red');
 $('li').next(3).css('color', 'orange');
 $('li > p').css('background', 'yellow');
@@ -11,11 +12,30 @@ $('li div div').css('background', 'blue');
 $('.indigo').css('color', 'indigo');
 $('.violet').css('color', 'violet');
 
-
+//second set of hw
 //first-child example use is really helpful!
 $('li p:first-child').html('<ul><li>Books</li><li>Ice Cream</li><li>Movies</li></ul>');
 $("#nameAwesome").text('Change this text to say "[Joe] is awesome!"');
 $("#twoDivs").html('<div>Put this sentence inside a div.</div><div>Put this sentence in a second div.</div>');
 $('#emptyString').text('');
 
+$('ul li:first-child a').attr('href', "https://www.google.com/").text('Google');
+$('ul li:nth-child(2) img').attr('src', "/static/img/deploy-yeti.jpg");
+$('#changeText').text($('#changeText').data('text')); //text is the name of data-text (3-2) week 5
 
+//third set of hw
+$('ul li:first-child').addClass('red');
+$('ul li:nth-child(2)').removeClass('blue');
+$('ul li').addClass('border');
+
+$('ul li:first-child').hover(function(){
+    $(this).toggleClass('blue');
+});
+
+$('ul li:nth-child(2)').on('click', function() {
+    $(this).toggleClass('blue')
+});
+
+$('ul li').hover(function(){
+    $(this).toggleClass('border');
+});
