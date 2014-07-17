257c81223f603a60b190ae5ae0870b52cbb50d70
Tsung Hung <hungmasterj@gmail.com>
2014-07-17 09:53:58 -0700

    today's exercise

---
+++ Week 5/w5d4.js
@@ -0,0 +1,10 @@
+/**
+ * Created by htm on 7/17/14.
+ */
+
+$('li').css('color', 'red');
+$('li').next(3).css('color', 'orange');
+$('li > p').css('background', 'yellow');
+$('li > div').css('background', 'green');
+$('.indigo').css('color', 'indigo');
+$('.violet').css('color', 'violet');
